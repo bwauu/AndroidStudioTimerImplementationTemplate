fabf12de84724d29fee2ecb402c8adbdc0c87415
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-20 17:51:33 +0100

    Initialized our field members and called setOnClick method on respective button. Implemented new methods of pauseTimer and startTimer

--- app/src/main/java/com/portofos/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/portofos/coundowntimerapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -24,5 +25,61 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewCountDown = findViewById(R.id.text_view_countdown);
+
+        mButtonStartPause = findViewById(R.id.button_start_pause);
+        mButtonReset = findViewById(R.id.button_reset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void startTimer() {
+        // Whenever startTimer is invoked, we will create a CountDownTimer object
+        // and then immediately start the CountDownTimer by start()
+
+        // first arg = length in milliseconds. second arg = how many milliseconds shall pass when
+        // our onTick method should be called. In our case every 1000 millisecond =
+        // 1 millisecond = 0.001 seconds. Therefore the onTick Method will be invoked every 1 Second.
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                // the parameter is amount of millis until this new CountDownTimer is finished.
+                // we save this in our member variable so we if it get cancelled we can continue
+                // on where we have stopped.
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+            }
+        }.start();
+        // After we have start our timer we need a reference to refer if we have started or not.
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void updateCountDownText() {
+    }
+
+    private void pauseTimer() {
     }
 }
