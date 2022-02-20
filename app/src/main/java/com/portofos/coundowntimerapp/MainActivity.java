5769566e670f66ca821b63790ff1da5f6a7e85aa
bwauu <benjamin.williamsappelberg.0315@student.uu.se>
2022-02-20 17:33:59 +0100

    Declared five instance variables and initialized 2 instance variables

--- app/src/main/java/com/portofos/coundowntimerapp/MainActivity.java
+++ app/src/main/java/com/portofos/coundowntimerapp/MainActivity.java
@@ -3,9 +3,23 @@ package com.portofos.coundowntimerapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
